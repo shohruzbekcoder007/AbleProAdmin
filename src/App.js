83a745f68831161d55f903b046f72c6277ac2ec2
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-03-17 14:34:28 +0300

    starter and react redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
+import React from "react"
+import { ThemeProvider } from "styled-components"
 import { ThemeProvider as MuiTheme } from '@mui/material/styles'
 import muiTheme from './theme/muiTheme'
-import defaultTheme from "./theme/defaultTheme";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Main } from "./pages";
+import defaultTheme from "./theme/defaultTheme"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import MainDrawer from "./layout/MainLayout/Drawer"
+import MainLayout from "./layout/MainLayout"
+import ThemeCustomization from './themes'
+import ScrollTop from './components/ScrollTop'
+import DashboardDefault from './pages/dashboard'
 
 function App() {
   return (
-    <MuiTheme theme={muiTheme}>
-      <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="login" element={<p>login</p>} />
-            <Route path="/" element={<Main/>} />
-            <Route path="*" element={<p>nage not found</p>} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </MuiTheme>
+    <ThemeCustomization>
+      {/* <ScrollTop> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<p>login</p>} />
+          <Route path="/" element={<MainLayout />} >
+            <Route index element={<DashboardDefault />} />
+          </Route>
+          <Route path="*" element={<p>nage not found</p>} />
+        </Routes>
+      </BrowserRouter>
+      {/* </ScrollTop> */}
+    </ThemeCustomization>
   );
 }
 
---
+++ src/layout/MainLayout/Drawer/DrawerContent/NavCard.js
@@ -0,0 +1,33 @@
+import { Button, Link, CardMedia, Stack, Typography } from '@mui/material';
+import { Logout } from 'iconsax-react';
+import avatar from '../../../../assets/images/analytics/welcome-banner.png';
+import AnimateButton from '../../../../components/@extended/AnimateButton';
+import MainCard from '../../../../components/MainCard';
+
+const NavCard = () => (
+  <MainCard sx={{ bgcolor: 'secondary.lighter', m: 3 }}>
+    <CardMedia component="img" image={avatar} />
+    <Stack alignItems="center" spacing={2.5}>
+      <Stack alignItems="center">
+        <Typography variant="h5">Able Pro</Typography>
+        <Typography variant="h6" color="secondary">
+          Checkout pro features
+        </Typography>
+      </Stack>
+      <AnimateButton>
+        <Button
+          variant="shadow"
+          color="warning"
+          component={Link}
+          href="https://1.envato.market/OrJ5nn"
+          target="_blank"
+          startIcon={<Logout />}
+        >
+          Upgrade to Pro
+        </Button>
+      </AnimateButton>
+    </Stack>
+  </MainCard>
+);
+
+export default NavCard;
